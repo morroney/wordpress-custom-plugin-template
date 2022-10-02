0e5fd192ac82f482eaafc98f86ff21f0ffb181c3
Dan <morroney@users.noreply.github.com>
2022-10-01 15:07:42 -1000

    Add files via upload

---
+++ dk-design-custom/dk-design-custom.php
@@ -0,0 +1,13 @@
+<?php
+//Add a layer to protect against snooping
+defined('ABSPATH') or die("No Bueno Amigo."); //Beat it!
+/*
+Plugin Name: DK Design Custom Plugin Template
+Plugin URI:  https://dkdesignhawaii.com
+Description: Custom starter template for plugin development.
+Version:     1.0
+Author:      Dan Morrone
+Author URI:  https://dkdesignhawaii.com
+*/
+
+// Hooks, actions, shortcodes, etc, below...
